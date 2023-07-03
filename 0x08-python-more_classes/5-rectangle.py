99b75fb322badb972ab0d052c4f68e656e672767
mbechi <agnessmminaa@gmail.com>
2023-07-03 20:25:27 +0000

    Python-More_Classes_and_Objects

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -72,4 +72,6 @@ class Rectangle:
         rect += ", " + str(self.__height) + ")"
         return (rect)
 
-    def __del__(self)
+    def __del__(self):
+        """Print a message for every deletion of a Rectangle."""
+        print("Bye rectangle...")
