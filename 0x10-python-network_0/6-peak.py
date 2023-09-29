456606058fa9fc2a6d78c07f3ed0f2c0949532b3
mbechi <agnessmminaa@gmail.com>
2023-09-29 18:37:37 +0000

    python-network_0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,32 +1,26 @@
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+
 def find_peak(list_of_integers):
-    """
-    Find a peak in a list of unsorted integers.
-    """
-    if not list_of_integers:
+    """ Finds the peak in a list of integers """
+    if list_of_integers == []:
         return None
 
-    low = 0
-    high = len(list_of_integers) - 1
+    length = len(list_of_integers)
+    m = int(length / 2)
+    li = list_of_integers
 
-    while low < high:
-        mid = (low + high) // 2
-        mid_value = list_of_integers[mid]
-        next_value = list_of_integers[mid + 1]
+    if m - 1 < 0 and m + 1 >= length:
+        return li[m]
+    elif m - 1 < 0:
+        return li[m] if li[m] > li[m + 1] else li[m + 1]
+    elif m + 1 >= length:
+        return li[m] if li[m] > li[m - 1] else li[m - 1]
 
-        if mid_value < next_value:
-            # The peak must be on the right side of mid
-            low = mid + 1
-        else:
-            # The peak must be on the left side of mid or mid itself
-            high = mid
+    if li[m - 1] < li[m] > li[m + 1]:
+        return li[m]
 
-    return list_of_integers[low]
-
-# Test cases
-if __name__ == "__main__":
-    print(find_peak([1, 2, 4, 6, 3]))          # Output: 6
-    print(find_peak([4, 2, 1, 2, 3, 1]))      # Output: 3
-    print(find_peak([2, 2, 2]))               # Output: 2
-    print(find_peak([]))                     # Output: None
-    print(find_peak([-2, -4, 2, 1]))         # Output: 2
-    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1])) # Output: 4
+    if li[m + 1] > li[m - 1]:
+        return find_peak(li[m:])
+    return find_peak(li[:m])
